36cbfafece01e88f49787c4723fea5b1c10a688c
hlx <honglixie35@outlook.com>
2025-05-02 20:53:10 +0800

    game

--- game.py
+++ game.py
@@ -1,4 +1,11 @@
-a=int(input())
-while(a<128):
-	print(a)
-	a=a<<1
+'''本项目于2025年4月19日，由谢泓黎建立，如果需要更改请保留声明'''
+import pygame
+import people
+pygame.init()
+size=500,500
+color=(250,250,250)
+screen=pygame.display.set_mode(size)
+screen.fill(color)
+pygame.display.set_caption("game")
+a=people.people(screen)
+i=0
---
+++ people.py
@@ -0,0 +1,8 @@
+import pygame
+class people:
+    num=0
+    a=pygame.image.load("picture/1.png")
+    def __init__(self,screen):
+        self.num+=1
+        aa=self.a.get_rect()
+        screen.blit(self.a,(0,0),aa)
